3e9349623ceccd89aff8eb786effdabbe2ae8b08
Amanda <ashendruk@users.noreply.github.com>
2017-06-27 07:25:48 -0400

    add scroll to names click

--- sidekick.js
+++ sidekick.js
@@ -1,9 +1,13 @@
 $(document).ready( function() {
-    // $("#stick").hide(); //hide your div initially
+	
     var topOfOthDiv = $("#othdiv").offset().top;
+    var topOfNames = $("#sidebar_text_names").offset().top;
     $(window).scroll(function() {
         if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
             $("#stick").show(500); //reached the desired point -- show div
+        }
+         if($(window).scrollTop() > topOfNames) { //scrolled past the other div?
+            $("#bubble_content").html("<span id='titleInsert'>Taking names?</span><br><span id='textInsert'>Click on a circle and I'll list gender-titled characters!</span>"); //reached the desired point -- show div
         }
         // $("#bubble_content").html("hello! This is a practice with my sidekick character! so cuuuuuute!");
     });
