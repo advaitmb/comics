aa47f2dc6abe131ecb2ee3fb3af678a5b1329546
Amanda <ashendruk@users.noreply.github.com>
2017-07-11 21:21:06 -0400

    clean up code

--- sidebar.js
+++ sidebar.js
@@ -1,16 +1,4 @@
 $(document).ready( function() {
-	
-    // var topOfOthDiv = $("#othdiv").offset().top;
-    // var topOfNames = $("#sidebar_text_names").offset().top;
-    // $(window).scroll(function() {
-    //     if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
-    //         $("#stick").show(500); //reached the desired point -- show div
-    //     }
-    //      if($(window).scrollTop() > topOfNames) { //scrolled past the other div?
-    //         $("#bubble_content").html("<span id='titleInsert'>Taking names?</span><br><span id='textInsert'>Click on a circle and I'll list gender-titled characters!</span><span id='textInsert_names'></span>"); //reached the desired point -- show div
-    //     }
-    // });
-
 
 
 $(window).scroll(function(){
