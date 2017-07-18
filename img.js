ec5334a87a14d3765806ea93755fba5726696308
Amanda <ashendruk@users.noreply.github.com>
2017-07-17 20:52:50 -0400

    remove

--- img.js
+++
@@ -1,7 +0,0 @@
-	$(function() {
-		  if($(window).width() <= 410) {
-		    $("#swarm").each(function() {
-		      $(this).attr("src", $(this).attr("src").replace("images/swarm.jpg", "images/swarm_mobile.jpg"));
-		    });
-		  }
-		});
