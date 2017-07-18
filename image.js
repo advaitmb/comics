49638c9e94b64ddac24eff0f2e4d95359fbfaa12
Amanda <ashendruk@users.noreply.github.com>
2017-07-17 20:53:35 -0400

    remove

--- image.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function(){
-//     var img_w = window.innerWidth;
-//     if(img_w <= 780){
-//       $(".picture").html("<img id='swarm' src='images/swarm_mobile.jpg'/>")
-//     }
-//     else if(img_w>780){
-//       $(".picture").html("<img id='swarm' src='images/swarm.png'/>")
-//     }
-// });
